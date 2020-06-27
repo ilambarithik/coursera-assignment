20313d1c69535bd28670ea1d82967aa3b95e7cfb
ilambarithik <66894108+ilambarithik@users.noreply.github.com>
2020-06-27 09:14:52 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function returns another 4 functions(closures) namely set, get, setinverse and getinverse.
+## set sets the data to the variables x and inv, whenever a fresh data is set, it assigns it to x and make the 'inv' variable NULL as it has tobe calculated fresh.
 
 makeCacheMatrix <- function(x = matrix()) {
 	 inv <- NULL
@@ -31,7 +29,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function calcualtes the inverse of the matrix supplied using solve function and assigs the value to the setinverse function of the makecachematrix. Together along
+## with the function above, if the matrix is new then it calculates the inverse first time; second time onwards it extracts it from the cache.
 
 cacheSolve <- function(x, ...) {
 	
